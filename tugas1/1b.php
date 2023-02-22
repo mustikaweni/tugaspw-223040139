0998ab4746278fc49e187e01d0279ac9ecb2294d
mustikaweni <mustikamustika492@gmail.com>
2023-02-22 23:26:28 +0700

    first commit

---
+++ tugas1/1b.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I am 72</title>
+</head>
+<body>
+    <?php
+        //2.Membuat 1 variable dari dua digit terahir dari NPM 
+        $x="39";
+        echo "Aku adalah angka $x</br>";
+        //dikali 5 
+         echo "jika aku dikali 5,maka aku sekarang menjadi"." ". $x *5 . "</br>";
+        //dibagi 2
+        echo "jika aku dibagi 2,maka aku sekarang". " " .$x*5/2 ."</br>";
+        //ditambah 7
+        echo "jika aku ditambah 75,maka aku sekarang menjadi"." ".$x*5/2+75 ."</br>";
+        //dikurang 20
+        echo "jika aku dikurangi 20,maka aku sekarang menjadi" . " " . $x*5/2+75-20 . "</br>";
+    
+    ?>
+
+</body>
+</html>
